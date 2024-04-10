732122a07b8deeb8590998905ef384fa943c45c9
vitorvhsilva <88109337+vitorvhsilva@users.noreply.github.com>
2024-04-10 12:51:56 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import ConteudoLateralEsquerda from './components/ConteudoLateralEsquerda.jsx'
+
+function App() {
+
+  return (
+    <>
+      <ConteudoLateralEsquerda />
+      
+      <div className='conteudo-central'>
+
+      </div>
+      <div className='conteudo-lateral-dir'>
+
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ConteudoLateralEsquerda.jsx
@@ -0,0 +1,14 @@
+import logo from '../assets/imgs/instagram-logo.png'
+
+export default function ConteudoLateralEsquerda() {
+  return (
+    <div className='conteudo-lateral-esq'>
+      <div className='logo-instagram'>
+        <img src={logo} alt=""/>
+      </div>
+      <div className='menu'>
+
+      </div>
+    </div>
+  )
+}
